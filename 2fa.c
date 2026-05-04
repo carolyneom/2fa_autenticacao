a254052db392e47ff505e78577bd4e6fcc34cefb
carolyneom <149268795+carolyneom@users.noreply.github.com>
2026-05-03 21:20:30 -0300

    Refactor 2FA code generation logic

--- 2fa.c
+++ 2fa.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Agora recebe o tempo como parâmetro
-int gerar_codigo_tempo(int secret, int timestep)
+int gerar_codigo(int secret, int timestep)
 {
-    int code = (timestep ^ secret) % 1000000;
+    long long mix = (long long)timestep * 1103515245 + secret;
+    int code = mix % 1000000;
 
     if (code < 0)
         code *= -1;
@@ -18,26 +18,21 @@ int main()
     int secret = 123456;
     int input;
 
-    // Captura o tempo UMA VEZ só
     time_t now = time(NULL);
-    int timestep_atual = now / 60;
+    int timestep = now / 60;
 
-    int codigo_atual = gerar_codigo_tempo(secret, timestep_atual);
-    int codigo_anterior = gerar_codigo_tempo(secret, timestep_atual - 1);
+    int codigo_atual = gerar_codigo(secret, timestep);
+    int codigo_anterior = gerar_codigo(secret, timestep - 1);
 
-    printf("Codigo atual (debug): %06d\n", codigo_atual);
+    printf("Codigo atual: %06d\n", codigo_atual);
 
     printf("Digite o codigo: ");
     scanf("%d", &input);
 
     if (input == codigo_atual || input == codigo_anterior)
-    {
         printf("Acesso liberado!\n");
-    }
     else
-    {
         printf("Codigo incorreto!\n");
-    }
 
     return 0;
 }
